829654d47921474615446cc8fc3e5d0f006bb539
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2020-12-16 03:43:26 -0700

    Add files via upload

--- Instalar.ps1
+++ Instalar.ps1
@@ -1,3 +1,7 @@
+# Execution policy
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+
 # Recive parameter "elevated"
 param([switch]$Elevated)
 
@@ -16,8 +20,6 @@ if ([Security.Principal.WindowsIdentity]::GetCurrent().Groups -notcontains 'S-1-
 $fullInstall = Read-Host -Prompt 'Do a full install? [y/N]'
 
 # Install chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Create app instalation string
